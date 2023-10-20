19f9944e5bcb155f1d9496fed105629de6276dbf
JLealc <jalealc.1577@gmail.com>
2023-10-19 18:10:37 -0600

    pickles for preprocessing

--- main.py
+++ main.py
@@ -2,105 +2,132 @@ import os
 import cv2
 import numpy as np
 from tqdm import tqdm
+from sklearn.externals import joblib  # For model saving
 import pickle
-from classification.classificationML import train_random_forest_classifier
-from features.feature_extraction import extract_features_from_segmented_regions
+from classification.classificationML import train_random_forest_classifier, train_svm_classifier_with_feature_selection
+from features.feature_extraction import extract_features_from_segmented_image , convert_to_padded_array, load_image_paths_from_folder
 from data_loader.data_loading import load_images_from_folder
-from segmentation.segmentation import perform_segmentation
 from preprocessing.preprocessing import *
+import random
+  
 
 class SkinImageClassifier:
-    def __init__(self, nevus_dir, others_dir):
+    def __init__(self, nevus_dir, others_dir, val_nevus_dir, val_others_dir):
         self.nevus_dir = nevus_dir
         self.others_dir = others_dir
-        self.output_folder = 'CAD_Challenge1/masks'
-        os.makedirs(self.output_folder, exist_ok=True)
+        self.val_nevus_dir = val_nevus_dir
+        self.val_others_dir = val_others_dir
+        self.preprocessed_image = self.load_preprocessed_image()
+
+    def load_preprocessed_image(self):
+        if os.path.exists('preprocessed_image.pickle'):
+            with open('preprocessed_image.pickle', 'rb') as file:
+                preprocessed_image = pickle.load(file)
+            return preprocessed_image
+        else:
+            return None
 
     def preprocess_image(self, image):
-          # Convert grayscale image to 3-channel color image if needed
-        if len(image.shape) == 2 or image.shape[2] == 1:
-            image_rgb = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+        if self.preprocessed_image is not None:
+            return self.preprocessed_image
+
+        resized_image = resize_images(image)
+        # Remove hair from the stretched grayscale image
+        hair_removed_image = remove_hair(resized_image)
+
+        # Save the preprocessed image to a pickle file
+        with open('preprocessed_image.pickle', 'wb') as file:
+            pickle.dump(hair_removed_image, file)
+
+        return hair_removed_image
+
+    def load_random_subset(self, folder_path, subset_size=None):
+        
+        # List all files in the folder
+        all_files = os.listdir(folder_path)
+
+        if subset_size is not None:
+            # Randomly select a subset of files
+            random_files = random.sample(all_files, subset_size)
         else:
-            image_rgb = image
-                
-        resized_image = cv2.resize(image_rgb, (700, 700))
+            # If subset_size is not specified, load all files
+            random_files = all_files
 
-        # Apply contrast stretching
-        contrast_stretched_image = contrast_stretching(resized_image)
+        # Load and preprocess the selected files
+        subset_images = []
+        for filename in random_files:
+            image_path = os.path.join(folder_path, filename)
+            preprocessed_image = self.preprocess_image(cv2.imread(image_path))
+            subset_images.append(preprocessed_image)
 
-        # Apply hair removal
-        hair_removed_image = remove_hair(contrast_stretched_image)
-
-        return hair_removed_image  # Return the preprocessed image
+        return subset_images
 
     def extract_features(self, image):
-        # Your feature extraction code here
-        feature_vector = extract_features_from_segmented_regions(image)  # Call the imported function
-        # Additional feature extraction steps if needed
+        feature_vector = extract_features_from_segmented_image(image) 
         return feature_vector
 
-    def process_images(self, class_dir, label):
-        # Check if preprocessed images exist in a Pickle file
-        pickle_file = f'preprocessed_images_{label}.pickle'
-        if os.path.isfile(pickle_file):
-            with open(pickle_file, 'rb') as file:
-                preprocessed_images = pickle.load(file)
+    def process_images(self, class_dir, label, subset_size=None):
+        if subset_size is not None:
+            images = self.load_random_subset(class_dir, subset_size)
         else:
-            images = load_images_from_folder(class_dir)
-            preprocessed_images = []
-
-            for img, filename in tqdm(images, desc=f"Processing {label} images", unit="image"):
-                preprocessed_image = self.preprocess_image(img)
-
-                # Save the preprocessed images as a Pickle file
-                preprocessed_images.append(preprocessed_image)
-            
-            with open(pickle_file, 'wb') as file:
-                pickle.dump(preprocessed_images, file)
+            image_files = load_image_paths_from_folder(class_dir)
+            images = [self.preprocess_image(cv2.imread(img_path)) for img_path in image_files]
 
         features = []
         labels = []
 
-        for preprocessed_image in preprocessed_images:
-            segmented_image = perform_segmentation(preprocessed_image)
-            features.extend(self.extract_features(segmented_image))
-            labels.extend([label] * len(features))
+        for img in tqdm(images, desc=f"Processing {label} images", unit="image"):
+            img_features = self.extract_features(img)  # Extract features for the whole image, not regions
 
-            # Save the processed image
-            output_path = os.path.join(self.output_folder, filename)
-            cv2.imwrite(output_path, segmented_image)
+            # Append the features and label for the entire image
+            features.append(img_features)
+            labels.append(label)
 
         return features, labels
+    
+    def train_classifier(self, subset_size=None, num_features_to_select=None):
+        nevus_features, nevus_labels = self.process_images(self.nevus_dir, label=0, subset_size=subset_size)
+        others_features, others_labels = self.process_images(self.others_dir, label=1, subset_size=subset_size)
 
-    def train_classifier(self):
-        nevus_features, nevus_labels = self.process_images(self.nevus_dir, label=0)
-        others_features, others_labels = self.process_images(self.others_dir, label=1)
-
-        # Convert lists of features to NumPy arrays
-        nevus_features = np.array(nevus_features)
-        others_features = np.array(others_features)
+        val_nevus_features, val_nevus_labels = self.process_images(self.val_nevus_dir, label=0, subset_size=subset_size)
+        val_others_features, val_others_labels = self.process_images(self.val_others_dir, label=1, subset_size=subset_size)
 
         # Combine features and labels for both classes
-        features = np.concatenate([nevus_features, others_features])
-        labels = np.concatenate([nevus_labels, others_labels])
+        train_features = nevus_features + others_features
+        train_labels = nevus_labels + others_labels
 
-        print(f"Total number of features: {len(features)}")
-        print(f"Total number of labels: {len(labels)}")
-        print(f"Features shape: {features.shape}")
-        print(f"Labels shape: {labels.shape}")
+        val_features = val_nevus_features + val_others_features
+        val_labels = val_nevus_labels + val_others_labels
 
-        if len(features) == 0:
+        # After combining features and labels, reshape the features array
+        train_features = np.array(train_features)  # Convert to NumPy array
+        train_features = train_features.reshape(train_features.shape[0], -1)  # Reshape to 2D
+
+        val_features = np.array(val_features)  # Convert to NumPy array
+        val_features = val_features.reshape(val_features.shape[0], -1)  # Reshape to 2D
+
+        print(f"Total number of train features: {len(train_features)}")
+        print(f"Total number of  train labels: {len(train_labels)}")
+
+        if len(train_features) == 0:
             print("Error: No features found.")
             return
-
-        # Train your classifier on all features and labels
-        rf_accuracy, rf_confusion_matrix = train_random_forest_classifier(features, labels)
+        
+        rf_accuracy, rf_confusion_matrix = train_random_forest_classifier(train_features, train_labels, val_features, val_labels)
 
         # Print classifier performance metrics
         print(f"Random Forest Accuracy: {rf_accuracy}")
         print(f"Random Forest Confusion Matrix:\n{rf_confusion_matrix}")
 
+        # Now, the features array is 2D, and you can proceed to train the classifier
+        svm_classifier, svm_accuracy, svm_report = train_svm_classifier_with_feature_selection(train_features, train_labels, val_features, val_labels, num_features_to_select)
+
+        # Print SVM classifier performance metrics
+        print(f"SVM Accuracy: {svm_accuracy}")
+        print(f"SVM Classification Report:\n{svm_report}")
 
 if __name__ == "__main__":
-    classifier = SkinImageClassifier(nevus_dir='train/train/nevus', others_dir='train/train/others')
-    classifier.train_classifier()
+    classifier = SkinImageClassifier(nevus_dir='C:/Users/Administrador/Documents/0. MAIA/3. Spain/1.CAD/1.Project_SEP/Skinbinary_train/nevus/', others_dir='C:/Users/Administrador/Documents/0. MAIA/3. Spain/1.CAD/1.Project_SEP/Skinbinary_train/others/',val_nevus_dir='C:/Users/Administrador/Documents/0. MAIA/3. Spain/1.CAD/1.Project_SEP/Skinbinary_val/nevus/', val_others_dir='C:/Users/Administrador/Documents/0. MAIA/3. Spain/1.CAD/1.Project_SEP/Skinbinary_val/others/')
+    subset_size = None
+    num_features_to_select = 200  # Choose the number of top features to select
+    classifier.train_classifier(subset_size=subset_size, num_features_to_select=num_features_to_select)
